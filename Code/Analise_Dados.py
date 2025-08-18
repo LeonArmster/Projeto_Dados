11e41740795239a8a569c7f2941091019364ebe8
LeonArmster <leo.armster1994@gmail.com>
2025-08-17 22:59:30 -0300

    Analise completada

--- Code/Analise_Dados.py
+++ Code/Analise_Dados.py
@@ -94,4 +94,46 @@ bar_graph(df_cliente['customer_state'])
 
 bar_graph(df_cliente['customer_city'])
 
-## Após analisar os gráficos podemos ver que os 3 estados que mais se vende são: SP, RJ, MG e que as cidades que mais se vendem são: São Paulo, Rio de Janeiro e Belo Horizonte
+### Após analisar os gráficos podemos ver que os 3 estados que mais se vende são: SP, RJ, MG e que as cidades que mais se vendem são: São Paulo, Rio de Janeiro e Belo Horizonte
+
+# Quarta pergunta: Qual a principal forma de pagamento?
+df_share_pagamento = df_pag.groupby('payment_type')[['order_id']].count()
+df_share_pagamento['Share'] = (df_share_pagamento['order_id'] / df_share_pagamento['order_id'].sum() * 100).round(1).astype(str) + '%'
+
+### Ao analisar o df share_pagamento podemos ver que 74% dos pagamentos foram feitos no cartão de crédito e 19% foi feito em boleto sendo os 7% restantes em outras formas de pagamento
+
+# há algum cliente que compra mais?
+bar_graph(df_cliente['customer_unique_id'])
+
+### Após analisar o gráfico podemos ver que não temos um cliente em destaque
+
+# Há algum cliente que tenha um valor de compra mais elevado em destaque?
+df_compras_gerais = pd.merge(df_ordens, df_itens, how='left')
+df_compras_gerais['total_price'] = df_compras_gerais['price'] + df_compras_gerais['freight_value']
+df_compras_gerais = pd.merge(df_compras_gerais, df_cliente, how='left')
+
+df_valor_clientes = df_compras_gerais.groupby('customer_unique_id')[['total_price']].sum()
+
+df_valor_clientes.describe()
+
+### Após analisar os valores de compra, vemos que não temos muitos valores de destaque, sendo a média 164,87 gasto tendo um outlier de 13664, porém mesmo no terceiro quartil, os valores ficam em 182,23 e no 1º quartil um valor de 62,39 indicando que os valores não estão desviando muito
+
+# Se possui datas de vendas houve aumento ou queda em algum produto ou categoria especifico em algum período?
+## Criando a coluna data
+df_ordens['purchase_date'] = pd.to_datetime(df_ordens['order_purchase_timestamp']).dt.date
+
+## Agrupando a quantidade de compras por dia
+df_qtd_compras_dia = df_ordens.groupby('purchase_date')[['order_id']].count()
+
+## Ordenando do maior para o menor para saber os dias que mais tiveram compras
+df_qtd_compras_dia.sort_values('order_id', ascending=False)
+
+### Podemos ver que as datas que mais venderam foram especificamente 24,25,26,27,28 e 29 de novembro de 2017, essa data foi blackfriday
+
+# Algum dia da semana vende mais?
+## Criando a coluna dia da semana
+df_ordens['day_name'] = pd.to_datetime(df_ordens['order_purchase_timestamp']).dt.day_name()
+
+df_ordens.groupby('day_name')[['order_id']].count().sort_values('order_id', ascending=False)
+
+### Ao analisar o dataframe, vemos que os dias que menos compram são nos finais de semana e o dia que mais compram é na segunda-feira
